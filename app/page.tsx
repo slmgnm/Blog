2de38c206474a2bb87e1b2caede0b9bad6782c3a
SALIM GHANEM <saleemghanem@gmail.com>
2023-06-25 01:50:36 +0300

    prisma DB auth google login

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col  justify-between p-24">
+      <h1 className='text-large'>Hello Next</h1>
+    </main>
+  )
+}
