a02a3de61c0adf597de3629d35a9c0645bad4ca5
SALIM GHANEM <saleemghanem@gmail.com>
2023-07-06 15:52:49 +0300

    populate data of posts with user info

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-'use client'
-import AddPost from "../app/components/AddPost"
+"use client";
+import axios from "axios";
+import AddPost from "../app/components/AddPost";
+import { useQuery } from "@tanstack/react-query";
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
+
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+  if (isLoading) return "loading";
+  if (error) return error;
+  console.log(data);
   return (
     <main className="">
       {/* <h1 className="text-large">Hello Next</h1> */}
-      <AddPost/>
+      <AddPost />
     </main>
   );
 }
---
+++ pages/api/posts/getPosts.tsx
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// import { AuthOptions } from "next-auth";
+import prisma from "../../../prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const result = await prisma.post.findMany({
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(403).json({ err: "Error fetching posts" });
+    }
+  }
+}
